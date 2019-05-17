fixed some bugs, still have gradient bugs to fix

dataset.py
@@ -23,17 +23,16 @@ class ParsingDataset(data.Dataset) :
         super(ParsingDataset, self).__init__()
         self.nameList = os.listdir(path+'/Feature')
         self.size = len(self.nameList)
-        print(self.size)
         self.path = path
 
     def __getitem__(self, index) :
         fileName = self.nameList[index]
-        feature = torch.from_numpy(np.loadtxt(self.path+'/Feature/'+fileName))
-        label = torch.from_numpy(np.loadtxt(self.path+'/Label/'+fileName))
-        neighbour = torch.from_numpy(np.loadtxt(self.path+'/Neighbour/'+fileName))
-        sequence = torch.from_numpy(np.loadtxt(self.path+'/Sequence/'+fileName))
-        number = torch.from_numpy(np.loadtxt(self.path+'/Number/'+fileName))
-        return feature, label, neighbour, sequence, number
+        feature = torch.from_numpy(np.loadtxt(self.path+'/Feature/'+fileName)).squeeze(0)
+        label = torch.from_numpy(np.loadtxt(self.path+'/Label/'+fileName)).squeeze(0)
+        neighbour = torch.from_numpy(np.loadtxt(self.path+'/Neighbour/'+fileName)).squeeze(0)
+        sequence = torch.from_numpy(np.loadtxt(self.path+'/Sequence/'+fileName)).squeeze(0)
+        number = torch.from_numpy(np.loadtxt(self.path+'/Number/'+fileName)).squeeze(0)
+        return [feature, label, neighbour, sequence, number]
 
     def __len__(self) :
         return self.size

main.py
@@ -11,4 +11,4 @@ if __name__ == "__main__" :
     testDataset = ParsingDataset("test")
     testLoader = DataLoader(dataset = testDataset, shuffle = True, pin_memory = torch.cuda.is_available())
 
-    lstm, lstmLoss = trainLSTM(trainLoader, numEpochs = 5)
+    lstm, lstmLoss = trainLSTM(trainLoader, numEpochs = 5, classNum = 18, featureSize = 64)

model.py
@@ -15,33 +15,36 @@ class GraphLSTMBlock(nn.Module) :
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
 
-    def step(self,input,hiddenState,cellState,index,neighbour,numNei) :
-        combined = torch.cat((input, hiddenState[index]),1)
-        neiHiddenState = torch.matmul(neighbour[i], hiddenSize) / numNei
+    def step(self,inp,hiddenState,cellState,index,neighbour,numNei) :
+        inp = inp.float()
+        combined = torch.cat((inp, hiddenState[index]),0)
+        neiHiddenState = torch.matmul(neighbour.double(), hiddenState.double()) / numNei
+        neiHiddenState = neiHiddenState.float()
         inputState = self.sigmoid(self.gate(combined) + self.neighbourGate(neiHiddenState))
-        forgetSelf = self.selfGate(input)
+        forgetSelf = self.selfGate(inp.float())
         forgetSelfMat = torch.Tensor(hiddenState.size(0), hiddenState.size(1))
-        forgetSelfMat = selfMat.copy_(forgetSelf)
+        forgetSelfMat = forgetSelfMat.copy_(forgetSelf)
         neiForgetState = self.sigmoid(forgetSelfMat + self.neighbourGate(hiddenState))
         forgetState = self.sigmoid(forgetSelf + self.neighbourGate(hiddenState[index]))
         outputState = self.sigmoid(self.gate(combined) + self.neighbourGate(neiHiddenState))
         helpCellState = self.tanh(self.gate(combined) + self.neighbourGate(neiHiddenState))
-        curCellState = torch.matmul(neighbour,neiForgetState * cellState) / numNei + forgetState * cellState[index] + inputState * helpCellState
+        curCellState = torch.matmul(neighbour.float(),neiForgetState.float() * cellState.float()) / numNei + forgetState.float() * cellState.float() + inputState.float() * helpCellState.float()
         curHiddenState = self.tanh(outputState * curCellState)
 
         return curCellState, curHiddenState
 
-   def forward(self,inputs,neighbour,numNei,sequence) :
-        cellState, hiddenState = initStates(inputs.size(0), inputs.size(1))
+    def forward(self,inputs,neighbour,numNei,sequence) :
+        cellState, hiddenState = self.initStates(inputs.shape[0], inputs.shape[1])
         residualHidden = hiddenState
         for index in sequence:
+            index = int(index.item())
             curCellState, curHiddenState = self.step(inputs[index], hiddenState, cellState[index], index, neighbour[index], numNei[index])
             cellState[index] = curCellState
             hiddenState[index] = curHiddenState
 
         return hiddenState + residualHidden
 
-    def initStates(size1,size2) :
+    def initStates(self,size1,size2) :
         useGPU = torch.cuda.is_available()
         if(useGPU) :
             hiddenState = Variable(torch.randn(size1,size2).cuda())

train.py
@@ -1,36 +1,39 @@
 import torch
+import torch.nn as nn
 import numpy as np 
 from model import GraphLSTMNet
 from torch.autograd import Variable
 
-def trainLSTM(trainLoader, numEpochs = 5, classNum) :
-    index, feature, label, neighbour, sequence, number = enumerate(trainLoader)
-    featureSize = feature.size(1)
+def trainLSTM(trainLoader, numEpochs, classNum, featureSize) :
     lstm = GraphLSTMNet(featureSize, featureSize, featureSize, classNum)
 
     useGPU = torch.cuda.is_available()
 
-    optimizer = optim.SGD(model.parameters(), lr = 0.0001, momentum=0.9)
+    optimizer = torch.optim.SGD(lstm.parameters(), lr = 0.0001, momentum=0.9)
 
-    #TODO: decide loss function here
+    criterion = nn.CrossEntropyLoss()
 
     for epoch in range(numEpochs):
-        for index, feature, label, neighbour, sequence, number in enumerate(trainLoader) :
+        for index, data in enumerate(trainLoader) :
+            torch.autograd.set_detect_anomaly(True)
+            feature, label, neighbour, sequence, number = data
+            label = label.long()
             if useGPU :
-                index = Variable(index.cuda())
                 feature = Variable(feature.cuda())
                 label = Variable(label.cuda())
+                sequence = Variable(sequence.cuda())
                 neighbour = Variable(neighbour.cuda())
                 number = Variable(number.cuda())
             else :
-                index = Variable(index)
-                feature = Variable(feature())
-                label = Variable(label())
-                neighbour = Variable(neighbour())
-                number = Variable(number())
+                feature = Variable(feature).squeeze()
+                label = Variable(label).squeeze()
+                sequence = Variable(sequence).squeeze()
+                neighbour = Variable(neighbour).squeeze()
+                number = Variable(number).squeeze()
 
-            outputs = GraphLSTMNet(feature, neighbour, number, sequence)
+            outputs = lstm(feature, neighbour, number, sequence)
             loss = criterion(outputs, label)
+            print(loss)
 
             optimizer.zero_grad()
             loss.backward()